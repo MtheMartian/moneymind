08c4ae901cc12f7e851163a7b14d9919f15f6433
MtheMartian <marvin.altidor@outlook.com>
2023-09-08 23:03:32 -0400

    in progress...

--- src/pages/MoneyManager.tsx
+++ src/pages/MoneyManager.tsx
@@ -11,139 +11,19 @@ function CustomTableSubCategories(){
 }
 
 
-function CustomTableBody(props: {tableCategoryMap: TypeCustomTable["categoryMap"], toggleEdit: boolean}){
-  const [showSubCategories, setShowSubCategories] = useState<JSX.Element | null>(null);
+function CustomTableBody(props: {tableCategoryMap: TypeCustomTable["categoryMap"]}){
   const [categories, setCategories] = useState<TypeCustomTable["categoryEntries"]>([]);
-  const [showInsertInputs, setShowInsertInputs] = useState<boolean>(false);
-
-  function deleteButtonHandler(e: React.SyntheticEvent<HTMLButtonElement, MouseEvent>){
-    const element: HTMLButtonElement = e.currentTarget;
-    const key: string | null = element.getAttribute("data-id");
-
-    if(key){
-      props.tableCategoryMap.delete(key);
-      console.log(...props.tableCategoryMap.entries());
-      setCategories(prev => prev = Array.from(props.tableCategoryMap.entries()));
-    }
-  }
-
-  // Check if map size is > 0 plus sign underneath category, else show inputs.
-
-  function InsertCategory(){
-    const categoryInput = useRef<HTMLInputElement>(null);
-    const amountInput = useRef<HTMLInputElement>(null);
-    const uId: string = uniqueId();
-
-    function addCategoryButtonHandler(){
-      props.tableCategoryMap.set(uId, {categoryInput.current!.value, Number(amountInput.current!.value)});
-  
-      categoryInput.current!.value = amountInput.current!.value = "";
-      setShowInsertInputs(prev => prev = false);
-      setCategories(prev => prev = Array.from(props.tableCategoryMap.entries()));
-    }
-
-    return(
-      <div>
-        <input type="text" placeholder="Category" ref={categoryInput}/>
-        <input type="text" placeholder="Amount" ref={amountInput}/>
-        <button onClick={addCategoryButtonHandler}>Add</button>
-      </div>
-    )
-  }
-
-  function uniqueId(): string{
-    return Date.now().toString(36) + Math.ceil(Math.random() * 1000000).toString(36);
-  }
-
-  useEffect(()=>{
-    setCategories(prev => prev = Array.from(props.tableCategoryMap.entries()));
-
-    return()=>{
-      setCategories(prev => prev = []);
-      setShowInsertInputs(prev => prev = false);
-    }
-
-  }, [props.tableCategoryMap, props.toggleEdit])
-
-  function displayInsertInputs(){
-    if(!showInsertInputs){
-      setShowInsertInputs(prev => prev = true);
-    }
-    else{
-      setShowInsertInputs(prev => prev = false);
-    }
-  }
-
-  function CategoryCells(props: {category: string, amount: number, uId: string, 
-                          tableCategoryMap: TypeCustomTable["categoryMap"]}){
-    const [toggleEdit, setToggleEdit] = useState<boolean>(false);
-    const categoryInput = useRef<HTMLInputElement>(null);
-    const amountInput = useRef<HTMLInputElement>(null);
-
-    function showCellOptions(): void{
-      if(!toggleEdit){
-        setToggleEdit(prev => prev = true);
-      }
-      else{
-        setToggleEdit(prev => prev = false);
-      }
-    }
-
-    function editCell(e: React.SyntheticEvent<HTMLButtonElement, MouseEvent>): void{
-      const key: string = e.currentTarget.getAttribute("data-id")!;
-      const updatedElement: {category: string, totalAmount: number} = {}
-
-      props.tableCategoryMap.set(key, updatedElement);
-      showCellOptions();
-    }
-
-    useEffect(()=>{
-      setToggleEdit(prev => prev = false);
-      
-      return()=>{
-        setToggleEdit(prev => prev = false);
-      }
-    }, []);
-
-    return(
-      <div data-id={props.uId}>
-        <div id={props.uId}>
-          <div onClick={showCellOptions}>
-            <input type="text" defaultValue={props.category} ref={categoryInput} />
-            <input type="text" defaultValue={props.amount} ref={amountInput} />
-          </div>
-          {toggleEdit ? 
-          <div>
-            <button data-id={props.uId} onClick={deleteButtonHandler}>X</button>
-            <button data-id={props.uId} onClick={editCell}>{">"}</button>
-          </div>: null}
-        </div>
-        {showSubCategories}
-      </div>
-    )
-  }
 
   return(
     <div>
-      {props.tableCategoryMap.size < 1 ?  <InsertCategory /> : null}
       {categories.map((category, index) =>
-      <div data-id={category[0]}>
-        <div id={category[0]}>
-          <div onClick={showCellOptions}>
-            <input type="text" defaultValue={props.category} ref={categoryInput} />
-            <input type="text" defaultValue={props.amount} ref={amountInput} />
-          </div>
-          {toggleEdit ? 
+        <div data-id={category[0]}>
           <div>
-            <button data-id={category[0]} onClick={deleteButtonHandler}>X</button>
-            <button data-id={category[0]} onClick={editCell}>{">"}</button>
-          </div>: null}
+            <input type="text" ref={categoryInput} />
+            <input type="text" ref={amountInput} />
+          </div>
         </div>
-        {index + 1 === categories.length ? null : <button onClick={displayInsertInputs}>{"+"}</button>}
-        {showSubCategories}
-      </div>
       )}
-      {showInsertInputs ? <InsertCategory /> : null}
     </div>
   )
 }
