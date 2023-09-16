5a6e9a3bdc1df9566dccb7846eaa2f1680a8b52e
MtheMartian <marvin.altidor@outlook.com>
2023-09-15 22:45:30 -0400

    in progress ...

--- src/components/navbar/Header.tsx
+++ src/components/navbar/Header.tsx
@@ -1,15 +1,36 @@
 import {Link} from 'react-router-dom';
+import { useState } from 'react';
 
-function LeftSideMenu(){
-  
+function KebabMenu(){
+  return(
+    <aside>
+
+    </aside>
+  )
 }
 
 export default function Header(){
+  const [kebabMenu, setKebabMenu] = useState<React.JSX.Element | null>(null);
+
+  function sideMenuHandler(): void{
+    if(kebabMenu){
+      setKebabMenu(prev => prev = null);
+    }
+    else{
+      setKebabMenu(prev => prev = <KebabMenu />);
+    }
+  }
+
   return(
     <header>
       <nav>
         <ul>
           <li>
+            <button id="kebab-menu" onClick={sideMenuHandler}>
+              <span>&#8901;</span>
+              <span>&#8901;</span>
+              <span>&#8901;</span>
+            </button>
             <Link to={""}>Home</Link>
           </li>
           <li>
@@ -17,6 +38,7 @@ export default function Header(){
           </li>
         </ul>
       </nav>
+      {kebabMenu}
     </header>
   )
 }
