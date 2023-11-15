70ee8948c1cb1019e7da00281adfecee1bca73b9
MtheMartian <marvin.altidor@outlook.com>
2023-11-14 23:01:52 -0500

    in progress...

--- src/pages/manager/calendar/Calendar.tsx
+++ src/pages/manager/calendar/Calendar.tsx
@@ -1,23 +1,62 @@
 import '../components/custom-calendar/custom-calendar.css';
+import {useEffect, useRef, useState, useMemo} from 'react';
 
 function Calendar(){
 
-  const dates: Date[] = [];
-  const todayDate: Date = new Date(Date.now());
-  const currentYear: number = todayDate.getFullYear();
-  const currentMonth: number = todayDate.getMonth();
+  //const [selectedDate, setSelectedDate] = useState<Date>(new Date(Date.now()));
+  const todayDate = useRef<Date>(new Date(Date.now()));
 
-  for(let j: number = 0; j < 31; j++){
-    const currentDate: Date = new Date(currentYear, currentMonth, j);
-    if(currentDate.getMonth() === currentMonth){
-      dates.push(currentDate);
+  const calendar = useRef<HTMLDivElement>(null);
+  const [currentYear, setCurrentYear] = useState<number>(todayDate.current.getFullYear());
+  const [currentMonth, setCurrentMonth] = useState<number>(todayDate.current.getMonth());
+
+
+  const dates = useMemo<Date[]>(()=>{
+
+    const datesArr: Date[] =[];
+    for(let j: number = 0; j <= 31; j++){
+      const currentDate: Date = new Date(currentYear, currentMonth, j);
+      if(currentDate.getMonth() === currentMonth){
+        datesArr.push(currentDate);
+      }
     }
+
+    return datesArr;
+  }, [currentYear, currentMonth]);
+
+  function returnMonthInt(month: string): number{
+    month = month.toLowerCase();
+    const monthsInt: Map<string, number> = new Map([["january", 0], ["february", 1], ["march", 2], 
+                                                    ["april", 3], ["may", 4], ["june", 5],
+                                                    ["july", 6], ["august", 7], ["september", 8], 
+                                                    ["october", 9], ["november", 10], ["december", 11]]);
+
+    return typeof monthsInt.get(month) !== "undefined" ? monthsInt.get(month)! : todayDate.current.getMonth();
   }
 
+
+  useEffect(()=>{
+    const currURL: URLSearchParams = new URL(window.location.href).searchParams;
+
+    if(currURL.has("year")){
+      setCurrentYear(prev => prev = Number(currURL.get("year")));
+    }
+
+    if(currURL.has("month")){
+      setCurrentMonth(prev => prev = returnMonthInt(currURL.get("month")!));
+    }
+
+    if(!currURL.has("year") && !currURL.has("month") && calendar.current){
+      const calendarItem = calendar.current.querySelector(`#calendar-item-wrapper-${todayDate.current.getDate()}`);
+      calendarItem!.scrollIntoView({behavior: "smooth", block: "nearest", inline: "start"});
+    }
+
+  }, [currentYear, currentMonth]);
+
   return(
-    <div id="calendar">
+    <div id="calendar" ref={calendar}>
       {dates.map(date =>
-      <div className="calendar-item-wrapper">
+      <div className="calendar-item-wrapper" id={`calendar-item-wrapper-${date.getDate()}`}>
         <div className="calendar-item-date">
           <p>{date.toDateString()}</p>
         </div>
