8f5867754d49bf2ef562e4c2f0bac7ca8071ccbd
MtheMartian <marvin.altidor@outlook.com>
2024-07-03 21:59:49 -0400

    working

---
+++ src/pages/dashboard/Graph.tsx
@@ -0,0 +1,32 @@
+function Bands(): JSX.Element{
+  return(
+    <div>
+      <div>Current</div>
+      <div>Goal</div>
+    </div>
+  )
+}
+
+function XAxis(): JSX.Element{
+  return(
+    <div>
+      <p></p>
+    </div>
+  )
+}
+
+function YAxis(): JSX.Element{
+  return(
+    <div>
+      <p></p>
+    </div>
+  )
+}
+
+function Dashboard(): JSX.Element{
+  return(
+    <div>
+      
+    </div>
+  )
+}
